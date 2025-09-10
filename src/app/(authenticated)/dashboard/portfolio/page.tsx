b0fc1a2f54eb4827c00824be1fd8bbf0dd00e2a2
ellamsg <ellamsgeorge7@gmail.com>
2025-09-10 22:22:41 +0100

    yujk

--- src/app/(authenticated)/dashboard/portfolio/page.tsx
+++ src/app/(authenticated)/dashboard/portfolio/page.tsx
@@ -888,14 +888,14 @@ const handleTierUpgrade = async () => {
               </>
             )}
             
-            {waitingForAccount && !creatingWallet && (
+            {/* {waitingForAccount && !creatingWallet && (
               <button
                 onClick={resetWalletCreation}
                 className="mt-6 bg-slate-700 hover:bg-slate-600 text-white py-2 px-6 rounded-xl font-medium transition-colors"
               >
                 Cancel Request
               </button>
-            )}
+            )} */}
           </div>
         </div>
       </div>
